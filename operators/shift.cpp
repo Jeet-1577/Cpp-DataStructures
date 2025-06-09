902fa04ffebaf5e9045da7569625c0d1420476c4
Jeet-1577 <mr.jac5333@gmail.com>
2025-06-09 16:47:19 +0530

    Add implementations for bitwise and shift operators with documentation

---
+++ operators/shift.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout<< (17>>1) << endl; // Right shift by 1 (divides by 2)
+    cout<< (17>>2 )<< endl; // Right shift by 2 (divides by 4)
+    cout<< (17<<1 )<< endl; // Left shift by 1 (multiplies by 2)
+    cout<< (17<<2 )<< endl; // Left shift by 2 (multiplies by 4)
+}
